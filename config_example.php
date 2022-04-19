a35efe15b5fd9074883b71467553541caba3132e
imradhe <radhesyam.musicals@gmail.com>
2022-04-19 15:29:28 +0530

    init

---
+++ config_example.php
@@ -0,0 +1,25 @@
+<?php
+$config = array(
+
+        'APP_NAME'=>'GrapheneTest',
+        'APP_MODE'=>'testing', //production/testing
+        'APP_TESTING_ROOT'=> 'GrapheneTest' , //only while testing
+        'APP_KEY'=>'',
+        'APP_DEBUG'=>'true',
+        'APP_URL'=>'http://localhost/GrapheneTest/',
+
+
+        'DB_CONNECTION'=>'mysql',
+        'DB_HOST'=>'localhost',
+        'DB_PORT'=>'3306',
+        'DB_DATABASE'=>'kautilyaeducation',
+        'DB_USERNAME'=>'root',
+        'DB_PASSWORD'=>'',
+
+        'MAIL_DRIVER'=>'smtp',
+        'MAIL_HOST'=>'smtp.mailtrap.io',
+        'MAIL_PORT'=>'2525',
+        'MAIL_USERNAME'=>'null',
+        'MAIL_PASSWORD'=>'null',
+        'MAIL_ENCRYPTION'=>'null'
+);
