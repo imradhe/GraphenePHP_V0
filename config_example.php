2a0e81d9badd1b5b2b22bad988aadc1164f8f298
imradhe <radhesyam.musicals@gmail.com>
2022-04-20 20:20:47 +0530

    Few changes in config

--- config_example.php
+++ config_example.php
@@ -3,17 +3,15 @@ $config  =[
 
         'APP_NAME'=>'GraphenePHP',
         'APP_TITLE'=>'GraphenePHP',
-        'APP_KEY'=>'',
-        'APP_DEBUG'=>'true',
         'APP_URL'=>'http://localhost/', // https://example.com/
-        'APP_SLUG'=>'graphenePHP', 
+        'APP_SLUG'=>'graphenephp', // full url: http://localhost/graphenephp
 
         // If the Graphene App is not hosted in the main directory of the domain add the directory name in APP_SLUG
         
         'DB_CONNECTION'=>'mysql',
         'DB_HOST'=>'localhost',
         'DB_PORT'=>'3306',
-        'DB_DATABASE'=>'graphene',
+        'DB_DATABASE'=>'kautilyaeducation',
         'DB_USERNAME'=>'root',
         'DB_PASSWORD'=>'',
 
@@ -28,9 +26,9 @@ $config  =[
         'APP_DESC' => 'App Description Goes here',
         'APP_SHORT_TITLE'=>'GraphenePHP',
         'APP_AUTHOR' => 'Radhe Shyam Salopanthula',
-        'APP_ICON' => 'URL', // Size 1000x1000
-        'APP_OG_ICON' => 'URL', // Size 600x300
-        'APP_OG_ICON_MOBILE' => 'URL', // Size 700x700
+        'APP_ICON' => 'assets/img/GraphenePHPIcon.png', // Size 1000x1000
+        'APP_OG_ICON' => 'assets/img/GraphenePHP.png', // Size 600x300
+        'APP_OG_ICON_MOBILE' => 'assets/img/GraphenePHP.png', // Size 700x700
         'APP_THEME_COLOR' => '#FFFFFF', // Color in HEX Code
         'APP_KEYWORDS' => 'GraphenePHP App, Radhe Shyam Salopanthula', // Max 20 Keywords 
         'APP_TWITTER_CREATOR' => '@imraadhe', // Twitter Username
