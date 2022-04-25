d531b2687a5fee3b54fdf3c4be733ed0a852612e
imradhe <radhesyam.musicals@gmail.com>
2022-04-25 12:43:58 +0530

    few changes

--- views/test.php
+++ views/test.php
@@ -1,4 +1,33 @@
-<?php 
-require('./db.php');
-$participants = mysqli_fetch_assoc(mysqli_query($con,"SELECT * FROM `students`"));
-echo json_encode($participants);
+<img src="" id="preview" />
+<input type="file" id="image">
+<canvas id="myCanvas"></canvas>
+
+<script>
+let image = document.querySelector("#image")
+let banner = document.querySelector("#tableBanner")
+let preview = document.getElementById('preview');
+
+    image.onchange = (event) => {
+        imgData = URL.createObjectURL(event.target.files[0])
+        var prevSrc = preview.src
+        preview.src = imgData
+    }
+    
+    preview.onload = () => {
+        preview.src = getBase64Image(preview)
+        console.log(preview)
+    }
+
+function getBase64Image(img) {
+  var canvas = document.createElement("canvas")
+  canvas.width = img.width
+  canvas.height = img.height
+  var ctx = canvas.getContext("2d")
+  ctx.drawImage(img, 0, 0)
+  var dataURL = canvas.toDataURL("image/png")
+  return dataURL
+}
+
+    
+
+</script>
