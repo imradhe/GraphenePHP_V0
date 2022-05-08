b0256b46958323c76b9922dd45a588d913656c97
imradhe <radhesyam.musicals@gmail.com>
2022-05-08 23:26:52 +0530

    Added getOS, getIP and getDevice functions

--- views/test.php
+++ views/test.php
@@ -1,33 +1,43 @@
-<img src="" id="preview" />
-<input type="file" id="image">
-<canvas id="myCanvas"></canvas>
+<?php
+$cookie_name = "auth";
+$cookie_value = md5("John Doe");
 
-<script>
-let image = document.querySelector("#image")
-let banner = document.querySelector("#tableBanner")
-let preview = document.getElementById('preview');
 
-    image.onchange = (event) => {
-        imgData = URL.createObjectURL(event.target.files[0])
-        var prevSrc = preview.src
-        preview.src = imgData
-    }
-    
-    preview.onload = () => {
-        preview.src = getBase64Image(preview)
-        console.log(preview)
-    }
+print_r(getDevice());
 
-function getBase64Image(img) {
-  var canvas = document.createElement("canvas")
-  canvas.width = img.width
-  canvas.height = img.height
-  var ctx = canvas.getContext("2d")
-  ctx.drawImage(img, 0, 0)
-  var dataURL = canvas.toDataURL("image/png")
-  return dataURL
-}
 
-    
+/*
+Users Table:
 
-</script>
+Name
+Email
+username (PK)
+role
+status
+password
+
+CREATE TABLE users (
+    username varchar(30) NOT NULL,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    role ENUM ('user','moderator','admin') DEFAULT 'user',
+    status int DEFAULT 0,
+    PRIMARY KEY (username)
+);
+
+
+logins table
+login ID = md5(username.time)
+username (FK)
+last login
+browser type
+ip address
+
+
+CREATE TABLE logs (
+    username varchar(30) NOT NULL,
+    loginID varchar(32) NOT NULL,
+    last TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    browser varchar(32) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username)
+);
