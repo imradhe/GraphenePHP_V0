bf2bc2ea876fb7f149616efa1fc480252ba1b32a
imradhe <radhesyam.musicals@gmail.com>
2022-05-09 23:21:54 +0530

    Updated Users Model

--- models/auth/users.sql
+++ models/auth/users.sql
@@ -1,8 +1,7 @@
-CREATE TABLE users (
-    username varchar(30) NOT NULL,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    role ENUM ('user','moderator','admin') DEFAULT 'user',
-    status int DEFAULT 0,
-    PRIMARY KEY (username)
-);
+CREATE TABLE `users` (
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('user','moderator','admin') DEFAULT 'user',
+  `status` int DEFAULT 0
+)
