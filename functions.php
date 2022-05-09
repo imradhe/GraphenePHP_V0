5d2e8354c3d3a28d28817a1b331ef246791a8ef6
imradhe <radhesyam.musicals@gmail.com>
2022-05-09 22:02:39 +0530

    Fixed auth

--- functions.php
+++ functions.php
@@ -166,6 +166,33 @@ function getDevice(){
     );
   } 
 
+function getSession(){
+  $loginID = $_COOKIE['auth'];
+
+  require_once("db.php");
+  $query = mysqli_fetch_assoc(mysqli_query($con,"SELECT * from logs where loginID='$loginID' and loggedout=0"));
+
+  if($query){
+    return $query;
+  }
+  else return false;
+}
+
+function getUser(){
+  $loginID = $_COOKIE['auth'];
+
+  require_once("db.php");
+  $query = mysqli_fetch_assoc(mysqli_query($con,"SELECT * from logs where loginID='$loginID' and loggedout=0"));
+
+  if($query){
+    $email = $query['email'];
+    $currentLog = mysqli_fetch_assoc(mysqli_query($con,"SELECT * from users where email='$email'"));
+    return $currentLog;
+  }
+  else return false;
+}
+
+
 function assets($path){
   echo "assets/".$path;
 }
