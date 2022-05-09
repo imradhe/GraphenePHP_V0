f0a40992791b38e2dd37a749797ba185d11e458f
imradhe <radhesyam.musicals@gmail.com>
2022-05-09 10:50:16 +0530

    testing auth

--- views/test.php
+++ views/test.php
@@ -1,8 +1,22 @@
 <?php
-$cookie_name = "auth";
-$cookie_value = md5("John Doe");
+$username = "arun";
+$password = "Arun@123";
 
 
+/** 
+ *
+ * if("arun" == $username && md5("Arun@123") == md5($password)){
+    $time = new DateTime();
+    $time = $time->format('Y-m-d H:i:s');
+    echo $time."<br>";
+    $auth = md5($username.md5($password).time());
+    setcookie("auth", $auth, time() + (86400 * 30), "/");
+}else echo "Invalid Login <br>";
+*/
+print_r($_COOKIE);
+
+if($_COOKIE['auth'] == md5($username.md5($password).time())) echo "Loggedin";
+else echo "Invalid Session";
 
 /*
 Users Table:
