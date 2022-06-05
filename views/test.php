869f0e5f0ca9196186c413d00f9b5712b83f4f09
imradhe <radhesyam.musicals@gmail.com>
2022-06-05 13:55:18 +0530

    Test File

--- views/test.php
+++ views/test.php
@@ -1,20 +1,3 @@
 <?php
-$username = "arun";
-$password = "Arun@123";
-
-
-/** 
- *
- * if("arun" == $username && md5("Arun@123") == md5($password)){
-    $time = new DateTime();
-    $time = $time->format('Y-m-d H:i:s');
-    echo $time."<br>";
-    $auth = md5($username.md5($password).time());
-    setcookie("auth", $auth, time() + (86400 * 30), "/");
-}else echo "Invalid Login <br>";
-*/
-print_r($_COOKIE);
-
-if($_COOKIE['auth'] == md5($username.md5($password).time())) echo "Loggedin";
-else echo "Invalid Session";
-
+header("Content-Type:Application/JSON");
+echo json_encode(["get"=> $_GET]);
