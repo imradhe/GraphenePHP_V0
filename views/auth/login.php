6e047bfec70bbf3f398e6b8028d6c150a92a6df2
imradhe <radhesyam.musicals@gmail.com>
2022-06-05 13:54:18 +0530

    Fixed Login

--- views/auth/login.php
+++ views/auth/login.php
@@ -10,19 +10,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content=""> 
-    <link href="../../assets/img/KEl.svg" rel="icon">
-    <link href="../../assets/img/KEl.svg" rel="apple-touch-icon">
-    <title>Login | Kautilya Education</title>
-<!-- CSS only -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
-<!-- JS, Popper.js, and jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <title>Login | Graphene PHP</title>
     <style>
       html,
 body {
@@ -70,6 +58,8 @@ body {
 }
     </style>
   </head>
+  
+  <?php include("views/partials/head.php"); ?>
   <body class="text-center">
     <form method="POST" name="Login" class="form-signin">
   <h1 class="h3 mb-3 font-weight-normal">Log In </h1>
