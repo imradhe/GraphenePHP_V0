7f0f8dbf5e7c0310b7f453227ed16e6d06398928
imradhe <radhesyam.musicals@gmail.com>
2022-06-05 13:54:59 +0530

    Fixed Lyrics

--- views/lyrics.php
+++ views/lyrics.php
@@ -1,9 +1,11 @@
 <?php
 locked();
 ?>
-<audio class="player" src="https://www.codepunker.com/resources/audio-sync-with-text/10bears.mp3" controls></audio>
 
-<!-- <video class="player" src="http://caiman.graodemilho.com.br/wp-content/uploads/2019/03/Video_SPLASH_v3_leve.mp4" controls></video> -->
+
+<a href="<?php assets("audio/10bears.mp3");?>" download="sample audio.mp3">Download</a>
+
+<audio class="player" src="<?php assets("audio/10bears.mp3");?>" controls></audio> 
 
 <div class="lyrics" style="display: none">
     0.125 | There were
@@ -11,7 +13,6 @@ locked();
     1.685 | and the little
     2.245 | one said
     2.985 | Roll over!
-    5.405 | 
 </div>
 
 <script>
@@ -31,7 +32,6 @@ lines.map((line, index) => {
 
 player.addEventListener('timeupdate', () => {
     syncData.forEach((item) => {
-        console.log(item)
         if (player.currentTime >= item.start) lyrics.innerText = item.text
     })
 })
