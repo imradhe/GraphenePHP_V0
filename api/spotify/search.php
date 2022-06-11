6950b49aa186b67833ffaa2775236a55fc0c1891
imradhe <radhesyam.musicals@gmail.com>
2022-06-11 14:56:08 +0530

    Removed spotify

--- api/spotify/search.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$url = str_replace(" ", "+",'https://api.spotify.com/v1/search?q='.$_GET['q'].'&type='.$_GET['type']);
-
-require('api/spotify/request.php');
-
-echo sendRequest($url, $token, "GET");
