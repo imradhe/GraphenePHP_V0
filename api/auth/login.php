56a369a7db8bc9a815f32ef43404a89fc1f4ef87
imradhe <radhesyam.musicals@gmail.com>
2022-06-11 14:55:19 +0530

    Fixed login

--- api/auth/login.php
+++ api/auth/login.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-require("api/controllers/AuthController.php");
+APIController('Auth');
 $auth = new Auth();
 
 echo $auth->login($email, $password);
